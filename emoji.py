a36d9ccf53688d461217d1a9695fba2406ab1f73
Jdiez84 <42740501+Jdiez84@users.noreply.github.com>
2020-07-20 12:35:44 -0400

    first project

---
+++ emoji.py
@@ -0,0 +1,33 @@
+import turtle 
+
+root=turtle.Turtle()
+root.up()
+root.goto(0,-100)
+root.down()
+
+root.begin_fill()
+root.fillcolor("yellow")
+root.circle(100)		#emoji pen
+root.end_fill()
+
+root.up()
+root.goto(-67,-40)		#width of pen 
+root.setheading(-60)
+root.width(5)
+root.down()
+root.circle(80,120)
+root.fillcolor("black")
+
+
+for i in range(-35,105,70):
+	root.up()
+	root.goto(i,35)				#for eyes
+	root.setheading(0)
+	root.down()
+	root.begin_fill()
+	root.circle(10)
+	root.end_fill()
+
+turtle.mainloop()
+
+
